ca0cdeb8d84a8f7d22e82986bfdf867d0a454c63
porfinogeneta <szyman123drum@gmail.com>
2022-08-27 16:47:58 +0200

    version 1.0

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 // router
-import { HashRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Course from "./pages/Course";
 
 function App() {
   return (
     <div className="App">
-        <HashRouter>
+        <BrowserRouter>
             <Navbar/>
             <h1>Welcome to flashcards</h1>
            <Routes>
                <Route element={<Home/>} path={"/"}></Route>
                <Route element={<Course/>} path={"/course/:lang/:amount"}></Route>
            </Routes>
-        </HashRouter>
+        </BrowserRouter>
 
     </div>
   );
